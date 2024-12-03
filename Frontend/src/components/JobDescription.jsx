d47960168ee638e16e01714d82d45c35189eb5cb
isuru kaldera <isuru.kaldera6425@gmail.com>
2024-12-03 19:25:20 +0530

    same

--- Frontend/src/components/JobDescription.jsx
+++ Frontend/src/components/JobDescription.jsx
@@ -45,6 +45,7 @@ const JobDescription = () => {
 
         try {
             const res = await axios.get(`${APPLICATION_API_END_POINT}/apply/${jobId}`, { withCredentials: true });
+            console.log('Fetching Details ...')
             if (res.data.success) {
                 const updatedSingleJob = {
                     ...singleJob,
