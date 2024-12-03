4a28435a61f49c414f269a762f1d36cca9b4468b
isuru kaldera <isuru.kaldera6425@gmail.com>
2024-12-03 16:02:05 +0530

    authenticated changes

--- Backend/routes/company.route.js
+++ Backend/routes/company.route.js
@@ -3,18 +3,18 @@ import isAuthenticated from "../Middle/isAuthenticated.js";
 import { getCompany, getCompanyById, registerCompany, updateCompany } from "../Control/Company.control.js";
 import { singleUpload } from '../Middle/multer.js';
 
-const router = express.Router();
+const router = express.Router(); 
 
 
-router.route("/register").post(registerCompany);
+router.route("/register").post(isAuthenticated, registerCompany); 
 
 
-router.route("/get").get(getCompany);
+router.route("/get").get(isAuthenticated, getCompany); 
 
 
-router.route("/get/:id").get(getCompanyById);
+router.route("/get/:id").get(isAuthenticated, getCompanyById); 
 
-router.route("/update/:id").put(singleUpload, updateCompany);
+router.route("/update/:id").put(isAuthenticated, singleUpload, updateCompany); 
 
 export default router;
 
--- Backend/routes/job.route.js
+++ Backend/routes/job.route.js
@@ -2,13 +2,13 @@ import express from "express";
 import isAuthenticated from "../Middle/isAuthenticated.js";
 import { postJob, getAllJobs, getAdminJobs, getJobById } from "../Control/Job.control.js";
 
-
+ 
 const router = express.Router();
 
-router.route("/post").post(postJob);
-router.route("/get").get(getAllJobs);
-router.route("/getadminjobs").get(getAdminJobs);
-router.route("/get/:id").get(getJobById);
+router.route("/post").post(isAuthenticated, postJob);
+router.route("/get").get(isAuthenticated, getAllJobs);
+router.route("/getadminjobs").get(isAuthenticated, getAdminJobs);
+router.route("/get/:id").get(isAuthenticated, getJobById);
 
 export default router;
 
