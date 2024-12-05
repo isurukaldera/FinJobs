f12ba24d959c8438b8717fb2e33812f1a01d1d97
isuru kaldera <isuru.kaldera6425@gmail.com>
2024-12-05 16:18:01 +0530

    change

--- Backend/Middle/isAuthenticated.js
+++ Backend/Middle/isAuthenticated.js
@@ -1,19 +1,26 @@
 import jwt from "jsonwebtoken";
 
 const isAuthenticated = async (req, res, next) => next();
-    
-const authenticate = (req, res, next) => {
-    const token = req.cookies.token || req.header('Authorization')?.replace('Bearer ', '');
-    if (!token) {
-        return res.status(401).json({ message: "Authentication required" });
-    }
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;  // Attach decoded user to request object
-        next();
-    } catch (err) {
-        return res.status(401).json({ message: "Invalid token" });
-    }
-};
-
+    // {
+    // try {
+    //     const token = req.cookies.token;
+    //     if (!token) {
+    //         return res.status(401).json({
+    //             message: "User not authenticated",
+    //             success: false,
+    //         })
+    //     }
+    //     const decode = await jwt.verify(token, process.env.SECRET_KEY);
+    //     if(!decode){
+    //         return res.status(401).json({
+    //             message:"Invalid token",
+    //             success:false
+    //         })
+    //     };
+    //     req.id = decode.userId;
+    //     next();
+    // } catch (error) {
+    //     console.log(error);
+    // }
+//}
 export default isAuthenticated;
