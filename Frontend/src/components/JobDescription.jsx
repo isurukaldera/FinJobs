bc52fc66bd5e01f31450f3b52e20c02a63e8f8ad
isuru kaldera <isuru.kaldera6425@gmail.com>
2024-12-05 10:30:39 +0530

    not the issue

--- Frontend/src/components/JobDescription.jsx
+++ Frontend/src/components/JobDescription.jsx
@@ -40,7 +40,8 @@ const JobDescription = () => {
     }, [jobId, dispatch, user?._id]);
 
     const applyJobHandler = async () => {
-        
+        console.log( 'appliedjobHandeler', isApplied )
+        if (isApplied) return; 
 
         try {
             const res = await axios.get(`${APPLICATION_API_END_POINT}/apply/${jobId}`, { withCredentials: true });
