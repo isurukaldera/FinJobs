c5911f5177dd9018ad870179102c7b7f09ebd8e0
isuru kaldera <isuru.kaldera6425@gmail.com>
2024-12-06 13:13:58 +0530

    again

--- Frontend/src/hooks/useGetAllJobs.jsx
+++ Frontend/src/hooks/useGetAllJobs.jsx
@@ -6,24 +6,33 @@ import { setAllJobs } from '../redux/jobSlice'
 
 const useGetAllJobs = () => {
     const dispatch = useDispatch();
-    const {searchedQuery} = useSelector(store=>store.job);
-    useEffect(()=>{
+    const { searchedQuery } = useSelector(store => store.job);
+
+    useEffect(() => {
         const fetchAllJobs = async () => {
             try {
                 const token = localStorage.getItem('token'); // Get the token from localStorage
-                const response = await axios.get(`${JOB_API_END_POINT}/get`, {
-                    headers: {
-                        'Authorization': `Bearer ${token}`,
-                    },
-                });
-                console.log(response.data);
+                if (token) {
+                    const response = await axios.get(`${JOB_API_END_POINT}/get`, {
+                        headers: {
+                            'Authorization': `Bearer ${token}`, // Add token to the header
+                        },
+                        withCredentials: true, // If needed for cookies
+                    });
+                    console.log(response.data); // Check this log to see the response
+                    if (response.data.success) {
+                        dispatch(setAllJobs(response.data.jobs)); // Assuming the jobs are in the 'jobs' field
+                    }
+                } else {
+                    console.log("No token found, cannot fetch jobs.");
+                }
             } catch (error) {
                 console.error("Error fetching jobs:", error);
             }
-        
-        }
+        };
+
         fetchAllJobs();
-    },[])
-}
+    }, [dispatch, searchedQuery]); // Add searchedQuery as a dependency if needed
+};
 
 export default useGetAllJobs    
--- Frontend/src/hooks/useGetAppliedJobs.jsx
+++ Frontend/src/hooks/useGetAppliedJobs.jsx
@@ -5,24 +5,35 @@ import { useDispatch } from "react-redux"
 import { APPLICATION_API_END_POINT } from "../utils/constant";
 
 const useGetAppliedJobs = () => {
-    const dispatch = useDispatch();
-  
-    useEffect(() => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
       const fetchAppliedJobs = async () => {
-        try {
-          const res = await axios.get(`${APPLICATION_API_END_POINT}/get`, { withCredentials: true });
-          console.log(res.data); // Check this log to see the structure of the response
-          if (res.data.success) {
-            dispatch(setAllAppliedJobs(res.data.application));
-          } else {
-            console.log('No applied jobs found or unsuccessful response');
+          try {
+              const token = localStorage.getItem('token'); // Get the token from localStorage
+              if (token) {
+                  const res = await axios.get(`${APPLICATION_API_END_POINT}/get`, {
+                      headers: {
+                          'Authorization': `Bearer ${token}`, // Include the token here
+                      },
+                      withCredentials: true,  // If needed for cookies
+                  });
+                  console.log(res.data); // Check this log to see the structure of the response
+                  if (res.data.success) {
+                      dispatch(setAllAppliedJobs(res.data.application));
+                  } else {
+                      console.log('No applied jobs found or unsuccessful response');
+                  }
+              } else {
+                  console.log("No token found, cannot fetch applied jobs.");
+              }
+          } catch (error) {
+              console.log('Error fetching applied jobs:', error);
           }
-        } catch (error) {
-          console.log('Error fetching applied jobs:', error);
-        }
       };
+
       fetchAppliedJobs();
-    }, [dispatch]); // Add dispatch as a dependency
-  };
+  }, [dispatch]); // Add dispatch as a dependency
+};
   
   export default useGetAppliedJobs;
