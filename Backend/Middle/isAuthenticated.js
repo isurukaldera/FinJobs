5b406199e2e9bb08c92a9d4a9bf1cc05d98a5dd2
isuru kaldera <isuru.kaldera6425@gmail.com>
2024-12-06 17:46:00 +0530

    no authentication

--- Backend/Middle/isAuthenticated.js
+++ Backend/Middle/isAuthenticated.js
@@ -1,31 +1,31 @@
 import jwt from "jsonwebtoken";
 
 const isAuthenticated = async (req, res, next) => {
-    try {
-        const token = req.cookies.token;
-        if (!token) {
-            return res.status(401).json({
-                message: "User not authenticated",
-                success: false,
-            });
-        }
+//     try {
+//         const token = req.cookies.token;
+//         if (!token) {
+//             return res.status(401).json({
+//                 message: "User not authenticated",
+//                 success: false,
+//             });
+//         }
 
-        const decoded = await jwt.verify(token, process.env.SECRET_KEY);
-        if (!decoded) {
-            return res.status(401).json({
-                message: "Invalid token",
-                success: false
-            });
-        }
+//         const decoded = await jwt.verify(token, process.env.SECRET_KEY);
+//         if (!decoded) {
+//             return res.status(401).json({
+//                 message: "Invalid token",
+//                 success: false
+//             });
+//         }
 
         
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            message: "Internal server error",
-            success: false
-        });
-    }
-}
+//     } catch (error) {
+//         console.log(error);
+//         return res.status(500).json({
+//             message: "Internal server error",
+//             success: false
+//         });
+//     }
+ }
 
 export default isAuthenticated;
