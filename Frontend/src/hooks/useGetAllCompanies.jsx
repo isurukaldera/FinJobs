13b296efd30f60bd9f6f314cebc8b45287098a6d
isuru kaldera <isuru.kaldera6425@gmail.com>
2024-12-07 02:35:54 +0530

    check

--- Frontend/src/hooks/useGetAllCompanies.jsx
+++ Frontend/src/hooks/useGetAllCompanies.jsx
@@ -9,8 +9,14 @@ const useGetAllCompanies = () => {
     const dispatch = useDispatch();
     useEffect(()=>{
         const fetchCompanies = async () => {
+            const token = localStorage.getItem('token');
             try {
-                const res = await axios.get(`${COMPANY_API_END_POINT}/get`,{withCredentials:true});
+                const res = await axios.get(`${COMPANY_API_END_POINT}/get`,{withCredentials:true},{
+                    headers: {
+                        Authorization: `Bearer ${token}`, // Send token in Authorization header
+                    },
+                    withCredentials: true, // If you're using cookies, keep this, otherwise remove it
+                });
                 console.log('called');
                 if(res.data.success){
                     dispatch(setCompanies(res.data.companies));
