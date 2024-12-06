11d8ae554d4cf7a8186d191ff234a4e52a881bad
isuru kaldera <isuru.kaldera6425@gmail.com>
2024-12-06 14:09:56 +0530

    try

--- Backend/index.js
+++ Backend/index.js
@@ -17,12 +17,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-const corsOptions = {
-    origin:'https://finjobs.onrender.com',
-    credentials:true
-}
 
-app.use(cors(corsOptions));
+
+app.use(cors({
+    origin: 'https://finjobs.onrender.com',  // Replace with your front-end URL
+    credentials: true, // To allow sending cookies along with the request
+}));
 
 const PORT = process.env.PORT || 3000;
 
