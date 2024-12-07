b9d35f3807f80ee4153ddae1b9fbef41b554d6bf
isuru kaldera <isuru.kaldera6425@gmail.com>
2024-12-07 14:22:54 +0530

    let see

--- Frontend/src/components/JobDescription.jsx
+++ Frontend/src/components/JobDescription.jsx
@@ -5,7 +5,7 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { APPLICATION_API_END_POINT, JOB_API_END_POINT } from '../utils/constant';
 import { setSingleJob } from '../redux/jobSlice';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { toast } from 'sonner';
 
 const JobDescription = () => {
