5beb79b41923054806e7a2200af1a67907a57142
isuru kaldera <isuru.kaldera6425@gmail.com>
2024-12-07 11:59:36 +0530

    checking

--- Backend/routes/application.route.js
+++ Backend/routes/application.route.js
@@ -6,6 +6,12 @@ import { applyJob, getApplicants, getAppliedJobs, updateStatus } from "../Contro
 const router = express.Router();
 
 router.route("/apply/:id").post(isAuthenticated, applyJob);
+const { jobId } = req.params;
+    // Validate and process the jobId here
+    if (!jobId) {
+        return res.status(404).send({ success: false, message: 'Job not found' });
+    }
+ 
 router.route("/get").get(isAuthenticated, getAppliedJobs);
 router.route("/:id/applicants").get(isAuthenticated, getApplicants);
 router.route("/status/:id/update").post(isAuthenticated, updateStatus);
