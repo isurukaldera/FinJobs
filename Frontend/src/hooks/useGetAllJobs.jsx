919548998da547bd864eb37f3e65eec0b83760e5
isuru kaldera <isuru.kaldera6425@gmail.com>
2024-12-07 13:48:12 +0530

    let check

--- Frontend/src/hooks/useGetAllJobs.jsx
+++ Frontend/src/hooks/useGetAllJobs.jsx
@@ -4,20 +4,14 @@ import { JOB_API_END_POINT } from '../utils/constant';
 import { useDispatch, useSelector } from 'react-redux';
 import { setAllJobs } from '../redux/jobSlice';
 
-
 const useGetAllJobs = () => {
     const dispatch = useDispatch();
-    const {searchedQuery} = useSelector(store=>store.job);
-    useEffect(()=>{
+    const { searchedQuery } = useSelector(store => store.job);
+
+    useEffect(() => {
         const fetchAllJobs = async () => {
-            const token = localStorage.getItem('token'); 
             try {
-                const res = await axios.get(`${JOB_API_END_POINT}/get?keyword=${searchedQuery}`, {
-                    headers: {
-                        Authorization: `Bearer ${token}`, // Send token in Authorization header
-                    },
-                    withCredentials: true, // If you're using cookies, keep this, otherwise remove it
-                });
+                const res = await axios.get(`${JOB_API_END_POINT}/get?keyword=${searchedQuery}`);
         
                 if (res.data.success) {
                     dispatch(setAllJobs(res.data.jobs));
@@ -27,7 +21,7 @@ const useGetAllJobs = () => {
             }
         };
         fetchAllJobs();
-    },[])
+    }, [searchedQuery, dispatch]); // Added searchedQuery to the dependency array to refetch when search changes
 }
 
 export default useGetAllJobs;
