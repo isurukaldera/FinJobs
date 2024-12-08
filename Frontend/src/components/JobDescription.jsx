3de5b8624dd3e8a844316be9eadf33a13a8b51e9
isuru kaldera <isuru.kaldera6425@gmail.com>
2024-12-08 18:06:43 +0530

    let see

--- Frontend/src/components/JobDescription.jsx
+++ Frontend/src/components/JobDescription.jsx
@@ -20,7 +20,11 @@ const JobDescription = () => {
 
     const applyJobHandler = async () => {
         try {
-            const res = await axios.get(`${APPLICATION_API_END_POINT}/apply/${jobId}`, {withCredentials:true});
+            const res = await axios.get(`${APPLICATION_API_END_POINT}/apply/${jobId}`, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`
+                },
+                withCredentials: true,});
             
             if(res.data.success){
                 setIsApplied(true); // Update the local state
